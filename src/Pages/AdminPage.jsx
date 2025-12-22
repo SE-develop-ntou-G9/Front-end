f61b3e255ad41d3be375391333ef99d5eaa219ad
Tony9737 <Tonyweng8437@gmail.com>
2025-12-22 15:21:12 +0800

    fix admin entry

--- src/Pages/AdminPage.jsx
+++ src/Pages/AdminPage.jsx
@@ -19,6 +19,12 @@ function AdminPage() {
     // console.log("Admin:", isAdmin);
 
     useEffect(() => {
+        if (isAdmin !== undefined && isAdmin !== '1') {
+            navigate("/");
+        }
+    }, [isAdmin, navigate]);
+
+    useEffect(() => {
         async function fetchPosts() {
             try {
                 const r = await fetch(API, { method: "GET" });
@@ -63,42 +69,7 @@ function AdminPage() {
             <div className="min-h-screen bg-gray-50">
                 <div className="max-w-2xl mx-auto px-4 pb-16">
 
-                    {/* 我留著標題 搜尋欄 和一些東西 要用就自己解開註解 */}
-
-                    {/* 搜尋欄 */}
-                    {/* <div className="mt-4">
-                        <div className="relative">
-                            <input
-                                type="text"
-                                placeholder="Search for a ride"
-                                className="w-full pl-4 pr-10 py-3 rounded-2xl bg-purple-100/60 placeholder-gray-500 outline-none"
-                            />
-                            <HiSearch className="absolute right-3 top-1/2 -translate-y-1/2 text-gray-600 text-xl" />
-                        </div>
-                    </div> */}
-
-                    {/* 標題那些的 */}
-                    {/* <div className="mt-5">
-                        <div className="mt-5 flex items-center justify-between">
-                            <div>
-                                <h2 className="text-base font-bold text-gray-900">最新共乘邀請</h2>
-                                <p className="text-xs text-gray-500 mt-0.5">查查看其他用戶的共乘請求</p>
-                            </div>
-
-                            {userRole === "車主" ? (
-                                <button
-                                    className="px-3 py-2 bg-black text-white rounded-lg hover:bg-gray-800 transition text-sm"
-                                    onClick={() => navigate("/uploadPost")}
-                                >
-                                    + 發布共乘貼文
-                                </button>
-                            ) : (
-                                <p className="text-xs text-gray-400 italic">
-                                    升級成車主後可發布共乘邀請
-                                </p>
-                            )}
-                        </div>
-                    </div> */}
+                    
                     <div className="flex gap-4 mt-6 mb-4">
                         <button
                             className="
